1df0c2db27edec4375f70540dc97659298e19398
Andika Yudha <andika.yudha@ukaea.uk>
2018-10-30 17:45:58 +0000

    firstCommit

---
+++ COBRA_APP/build/src/CMakeFiles/MinimalQml.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/build/src/MinimalQml_automoc.cpp" "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/build/src/CMakeFiles/MinimalQml.dir/MinimalQml_automoc.cpp.o"
+  "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/src/backend.cpp" "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/build/src/CMakeFiles/MinimalQml.dir/backend.cpp.o"
+  "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/src/main.cpp" "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/build/src/CMakeFiles/MinimalQml.dir/main.cpp.o"
+  "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/build/src/qrc_qml.cpp" "/media/ayudha/workspace/workspace/COBRA-GUI/COBRA_APP/build/src/CMakeFiles/MinimalQml.dir/qrc_qml.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_QML_LIB"
+  "QT_QUICK_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/include"
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/include/QtWidgets"
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/include/QtGui"
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/include/QtCore"
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/./mkspecs/linux-g++"
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/include/QtQml"
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/include/QtNetwork"
+  "/media/ayudha/workspace/Qt/5.12.0/gcc_64/include/QtQuick"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ COBRA_APP/build/src/CMakeFiles/MinimalQml.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "MinimalQml_automoc.cpp"
+  "qrc_qml.cpp"
+  "CMakeFiles/MinimalQml.dir/backend.cpp.o"
+  "CMakeFiles/MinimalQml.dir/main.cpp.o"
+  "CMakeFiles/MinimalQml.dir/qrc_qml.cpp.o"
+  "CMakeFiles/MinimalQml.dir/MinimalQml_automoc.cpp.o"
+  "MinimalQml.pdb"
+  "MinimalQml"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinimalQml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
